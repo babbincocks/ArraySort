7abc9273f39ba7dfd221b36e25c6744a2289848f
babbincocks <zabbybab@gmail.com>
2018-05-01 20:46:13 -0400

    Finished everything code-wise for the Sort function. Now to do Search...

--- ArraySort/ArraySort/Form1.cs
+++ ArraySort/ArraySort/Form1.cs
@@ -19,7 +19,8 @@ namespace ArraySort
         }
         StreamReader source = new StreamReader(@"..\Debug\Names.csv");
         string[] names = new string[5000];
-
+        PersonName[] splitNames = new PersonName[5000];
+        bool sort = false;
 
         private void frmMain_Load(object sender, EventArgs e)
         {
@@ -28,7 +29,10 @@ namespace ArraySort
             while (!source.EndOfStream && index < names.Length)
             {
                 names[index] = source.ReadLine().ToString();
-                lbNameList.Items.Add(names[index]);
+                string[] nameSplit = names[index].Split(',');
+                splitNames[index] = new PersonName(nameSplit[0].ToString(), nameSplit[1].ToString());
+
+                lbNameList.Items.Add(splitNames[index]);
                 index++;
             }
             DateTime end = DateTime.Now;
@@ -41,7 +45,23 @@ namespace ArraySort
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (sort == false)
+                {
+                    throw new Exception("Please sort the names first.");
+                }
+                else
+                {
+                    string searchTerm = txtNameSearch.Text;
 
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void sortToolStripMenuItem_Click(object sender, EventArgs e)
@@ -51,52 +71,57 @@ namespace ArraySort
                 DateTime startTime = DateTime.Now;
 
                 int minIndex;
-                string minValue;
+                PersonName minValue;
 
-                for (int start = 0; start < names.Length - 1; start++)
-                {
-
-                    minIndex = start;
-                    minValue = names[start];
-
-
-                    for (int index = start + 1; index < names.Length; index++)
+                    for (int start = 0; start < splitNames.Length - 1; start++)
                     {
-                        if (String.Compare(minValue, names[index], true) == 1)
+
+                        minIndex = start;
+                        minValue = splitNames[start];
+
+
+                        for (int index = start + 1; index < splitNames.Length; index++)
                         {
-                            minValue = names[index];
-                            minIndex = index;
+                            if (String.Compare(splitNames[index].ToString(0), minValue.ToString(0), true) == 0)
+                            {
+
+                                if (String.Compare(minValue.ToString(1), splitNames[index].ToString(1), true) == 1)
+                                {
+                                    minValue = splitNames[index];
+                                    minIndex = index;
+                                }
+                            }
+                            else if (String.Compare(minValue.ToString(0), splitNames[index].ToString(0), true) == 1)
+                            {
+                                minValue = splitNames[index];
+                                minIndex = index;
+                            }
+
                         }
 
+                        Swap(ref splitNames[minIndex], ref splitNames[start]);
+
+
+
                     }
-
-                    Swap(ref names[minIndex], ref names[start]);
-
-
-                }
+                
 
                 lbNameList.Items.Clear();
 
-                for (int index = 0; index <= names.Length - 1; index++)
+                for (int index = 0; index <= splitNames.Length - 1; index++)
                 {
-                    lbNameList.Items.Add(names[index]);
+                    lbNameList.Items.Add(splitNames[index]);
                 }
 
                 DateTime endTime = DateTime.Now;
                 TimeSpan finalTime = endTime - startTime;
-                if (finalTime.Milliseconds >= 1000)
-                {
+
                     string time = "Sorting the names took " + finalTime.Seconds + " seconds and " + finalTime.Milliseconds + " milliseconds.";
                     lbTimeRecords.Items.Add(time);
-                }
-                else if (finalTime.Milliseconds < 1000)
-                {
-                    string time = "Sorting the names took " + finalTime.Milliseconds + " milliseconds.";
-                    lbTimeRecords.Items.Add(time);
-                }
+
 
                 lbTimeRecords.TopIndex = lbTimeRecords.Items.Count - 1;
-
+                sort = true;
             }
             catch (Exception ex)
             {
@@ -104,9 +129,58 @@ namespace ArraySort
             }
         }
 
-        private void Swap(ref string a, ref string b)
+
+        public class PersonName
         {
-            string placeholder = a;
+
+            string firstName;
+            string lastName;
+
+            public PersonName(string last, string first)
+            {
+                lastName = last;
+                firstName = first;
+            }
+
+            public string last
+            {
+                get { return lastName; }
+                set { lastName = value; }
+            }
+
+            public string first
+            {
+                get { return firstName; }
+                set { firstName = value; }
+            }
+
+            public override string ToString()
+            {
+                return last + "," + first;
+            }
+
+            public string ToString(int which)
+            {
+                if (which == 0)
+                {
+                    return last;
+                }
+                else if (which == 1)
+                {
+                    return first;
+                }
+                else
+                {
+                    return last + "," + first;
+                }
+            }
+
+        }
+
+
+        private void Swap(ref PersonName a, ref PersonName b)
+        {
+            PersonName placeholder = a;
             a = b;
             b = placeholder;
 
@@ -117,14 +191,14 @@ namespace ArraySort
             try
             {
                 DateTime start = DateTime.Now;
-                //int index = 0;
-                //StreamWriter output = new StreamWriter(@"..\Debug\SortedNames.csv");
+
+                for(int index = 0; index < splitNames.Length; index++)
+                {
+                    names[index] = splitNames[index].ToString();
+                }
+
                 File.WriteAllLines(@"..\Debug\SortedNames.csv", names);
-                //while (index < names.Length)
-                //{
-                //    output.WriteLine(names[index].ToString());
-                //    index++;
-                //}
+
                 DateTime end = DateTime.Now;
                 TimeSpan length = end - start;
 
